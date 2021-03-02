83b8b6d551dd06bbd17481e780b5b1d1127fc885
P0458 <p0458@student.jamk.fi>
2021-03-02 18:45:34 +0200

    http-c

--- components/ListItem.js
+++ components/ListItem.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 
+const uploadUrl = 'http://media.mw.metropolia.fi/wbma/uploads/';
+
 const ListItem = (props) => {
   // console.log(props);
   return (
@@ -9,7 +11,7 @@ const ListItem = (props) => {
       <View style={styles.imageBox}>
         <Image
           style={styles.image}
-          source={{uri: props.singleMedia.thumbnails.w160}}
+          source={{uri: uploadUrl + props.singleMedia.thumbnails.w160}}
         />
       </View>
       <View style={styles.textbox}>
